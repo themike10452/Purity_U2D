5ba1ff66101c3c44426f7e0c1f69124d34e3a2be
themike10452 <thephenommvp@gmail.com>
2015-02-21 15:06:01 +0200

    avoid NPE

--- PurityU2D/src/main/java/lb/themike10452/PurityU2D/BootCompleteBroadcastReceiver.java
+++ PurityU2D/src/main/java/lb/themike10452/PurityU2D/BootCompleteBroadcastReceiver.java
@@ -13,7 +13,7 @@ import lb.themike10452.PurityU2D.Services.BackgroundAutoCheckService;
 public class BootCompleteBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction() == Intent.ACTION_BOOT_COMPLETED) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             SharedPreferences preferences = context.getSharedPreferences("Settings", Context.MODE_MULTI_PROCESS);
             if (preferences.getBoolean(Keys.KEY_SETTINGS_AUTOCHECK_ENABLED, true))
                 context.startService(new Intent(context, BackgroundAutoCheckService.class));
